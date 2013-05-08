d6f7ae6d8a18fed609acb36fd68002c714951595
KMuncie <nivek2002@gmail.com>
2013-05-07 23:00:22 -0700

    Added Random Image code to Page.php, not yet working. Syntax error

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -10,6 +10,43 @@ class Page extends SiteTree {
 }
 class Page_Controller extends ContentController {
 
+public function getImagesFromDir($path) {
+    $images = array();
+    if ( $img_dir = @opendir($path) ) {
+        while ( false !== ($img_file = readdir($img_dir)) ) {
+            // checks for gif, jpg, png
+            if ( preg_match("/(\.gif|\.jpg|\.png)$/", $img_file) ) {
+                $images[] = $img_file;
+            }
+        }
+        closedir($img_dir);
+    }
+    return $images;
+}
+
+public function getRandomFromArray($ar) {
+    mt_srand( (double)microtime() * 1000000 ); // php 4.2+ not needed
+    $num = array_rand($ar);
+    return $ar[$num];
+}
+
+/////////////////////////////////////////////////////////////////////
+// This is the only portion of the code you may need to change.
+// Indicate the location of your images 
+
+public static $root = 'letspoop';
+// use if specifying path from root
+// $root = $_SERVER['DOCUMENT_ROOT'];
+
+public static $path = 'assets/Uploads/cats/';
+
+// End of user modified section 
+/////////////////////////////////////////////////////////////////////
+
+// Obtain list of images from directory 
+public static $imgList = getImagesFromDir($root . $path);
+public static $img = getRandomFromArray($imgList);
+
 	/**
 	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
 	 * permissions or conditions required to allow the user to access it.
--- themes/simple/templates/Layout/Page.ss
+++ themes/simple/templates/Layout/Page.ss
@@ -1,6 +1,6 @@
 <div class="content-container unit size4of5 lastUnit">
 	<article>
 		<h1>$Title</h1>
-		<div class="content"><img src="$src" alt="" /></div>
+		<div class="content"><img src="$img" alt="" /></div>
 	</article>
 </div>
